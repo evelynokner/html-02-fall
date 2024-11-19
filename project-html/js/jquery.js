2494523eb95a497da28f7527c5e0c0540049cb41
evelynokner <oknerevelyn@gmail.com>
2024-11-19 16:05:49 -0500

    Add files via upload

---
+++ project-html/js/jquery.js
@@ -0,0 +1,10 @@
+// global header
+$(document).ready(function(){
+    $(function(){
+        // load template html documents
+        $("#head").load("./includes/meta.html");
+        $("#header").load("./includes/header.html");
+        $("#footer").load("./includes/footer.html");
+    });
+})
+    
