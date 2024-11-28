b500836c84f8471e978a451dfd7a1d15d25577c1
evelynokner <oknerevelyn@gmail.com>
2024-11-27 20:09:27 -0500

    Add files via upload

--- project-html/js/custom-js.js
+++ project-html/js/custom-js.js
@@ -1,8 +1,8 @@
-// global header, footer, & metadata
+// global header & footer
 $(document).ready(function(){
     $(function(){
         // load template html documents
-        $("#common-meta").load("./includes/meta.html");
+        //$("#common-meta").load("./includes/meta.html");
         $("#header").load("./includes/header.html");
         $("#footer").load("./includes/footer.html");
     });
